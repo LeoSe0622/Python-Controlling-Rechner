87b7621cb9da6265d578370e5efded6a8e32f183
LeoSE <Leonard.Schulz-Ebschbach@Student.HTW-Berlin.de>
2026-09-02 17:26:27 +0200

    fix für date Umschreibung

--- kennzahlen.py
+++ kennzahlen.py
@@ -9,20 +9,24 @@ import csv
 from datetime import datetime
 
 def main():
+
     parser = argparse.ArgumentParser(description="Kennzahlen aus einer Buchungs-CSV berechnen.")
     parser.add_argument("csv_pfad", help="Pfad zur Buchungs-CSV-Datei")
 
     args = parser.parse_args()
 
     csv_pfad = args.csv_pfad
+
     try:
         with open(csv_pfad, encoding="utf-8", newline = "") as datei:
             leser = csv.DictReader(datei, delimiter=";")
             spalten = leser.fieldnames
             buchungen = list(leser)
+
     except FileNotFoundError:
         print(f"Fehler: Datei nicht gefunden: {csv_pfad}", file=sys.stderr)
         sys.exit(1)
+
     if spalten is None:
         print(f"Fehler: Keine Spalten in der CSV-Datei gefunden: {csv_pfad}", file=sys.stderr)
         sys.exit(1)
@@ -30,16 +34,22 @@ def main():
     print(f"Datei: {csv_pfad}")
     print(f"Spalten: {', '.join(spalten)}")
     print(f"{len(buchungen)} Buchungen gelesen")
+
     if "betrag" in spalten:
         for buchung in buchungen:
-            buchung["betrag"] = float(buchung["betrag"].replace(",", "."))
-            print(buchung["kategorie"], "-> ", buchung["betrag"])
+            try: 
+                buchung["betrag"] = float(buchung["betrag"].replace(".", "").replace(",", "."))
+            except ValueError:
+                 print(f"Fehler: Ungültiger betrag")
+                 sys.exit(1)
     if "datum" in spalten:
         for buchung in buchungen:
             try:
                 buchung["datum"] = datetime.strptime(buchung["datum"], "%d.%m.%Y").date()
+
             except ValueError:
                 print(f"Fehler: Ungültiges Datum: {buchung['datum']}", file=sys.stderr)
                 sys.exit(1)
+
 if __name__ == "__main__":
     main()
